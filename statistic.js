e554c5a7abff1483de30d3908a25163627c5bc88
Thomas Fritzsche <tf@noto.de>
2025-02-14 23:28:09 +0100

    Add little more statistics

--- statistic.js
+++ statistic.js
@@ -1,5 +1,4 @@
-
-
+// This class keeps statistical records on all test results.
 
 export class Statistic {
     static statistic_store_key = 'afu_test_statistic'
@@ -9,13 +8,28 @@ export class Statistic {
             return Statistic._instance
         }
         Statistic._instance = this
-
+        this._questions = new Object()
         this._statistic = {
-            records: []
+            records: new Array()
         }
         this.load()
 
+
+
     }
+
+    _applyQuestion(question, rating) {
+        if (!this._questions[question]) this._questions[question] = 0
+        this._questions[question] += rating
+        if (this._questions[question] < 0) this._questions[question] = 0
+    }
+
+    _applyResult(result) {
+       result.correct.forEach( question => this._applyQuestion(question, 1) )
+       const delta_incorrect =  - 1000
+       result.incorrect.forEach( question => this._applyQuestion(question, delta_incorrect) )                 
+    }
+
     store() {
         localStorage.setItem(Statistic.statistic_store_key, JSON.stringify(this._statistic))
     }
@@ -27,10 +41,14 @@ export class Statistic {
             this._statistic = statistic
             if (statistic) this._statistic = { ...this._statistic, ...statistic }
         }
+       this._statistic.records.forEach(r  => {
+          this._applyResult(r)
+       })
     }
 
     addResults(result) {
         this._statistic.records.push(result)
+        this._applyResult(result)
         this.store()
     }
 
